6b349bfbf97d1c5d3ece4bcafe4234d15b7c590b
agermel <agermel@mails.ccnu.edu.cn>
2025-05-25 01:37:15 +0800

    ✨ 在游戏界面按esc进入设置界面

--- scripts/main/PlayerStates.cs
+++ scripts/main/PlayerStates.cs
@@ -6,10 +6,10 @@ using System.Threading.Tasks;
 
 public class PlayerStates
 {
-    public string UserName { get; set; } = "";
-    public float Health { get; set; } = 100;
-    public float MaxSpeed { get; set; } = 800f;
-    public float HPRegenerationRate { get; set; } = 10f;
-    public int Score { get; set; } = 0;
-    public int CurrentMoney { get; set; } = 0;
+	public string UserName { get; set; } = "";
+	public float Health { get; set; } = 100;
+	public float MaxSpeed { get; set; } = 800f;
+	public float HPRegenerationRate { get; set; } = 10f;
+	public int Score { get; set; } = 0;
+	public int CurrentMoney { get; set; } = 0;
 }
